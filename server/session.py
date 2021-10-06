95d245eb0aa59fed01cc3886df4770d8d5dd4ac4
VictorJimenez99 <jimenezvictor99@gmail.com>
2021-10-05 22:03:27 -0500

    Bug fix in session.py

--- server/session.py
+++ server/session.py
@@ -6,10 +6,13 @@ from server.orm import LoginCookie
 def has_valid_session(request):
     access_key = request.cookies.get("access_key", "empty")
     if access_key != "empty":
+        cookie: LoginCookie = LoginCookie().find_cookie_by_value(access_key)
+        print("found:" + str(cookie))
+        if cookie.expiration_date < int(time.time()):
+            return False
         return True
 
     cookie: LoginCookie = LoginCookie().find_cookie_by_value(access_key)
-
     if cookie is None:
         return False
 
