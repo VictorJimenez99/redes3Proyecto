bd2333dd6b37aa96fb529f9b5f55e5fb1e3b7048
VictorJimenez99 <jimenezvictor99@gmail.com>
2021-10-05 19:12:37 -0500

    Utility functions for sessions

---
+++ server/session.py
@@ -0,0 +1,19 @@
+import time
+
+from server.orm import LoginCookie
+
+
+def has_valid_session(request):
+    access_key = request.cookies.get("access_key", "empty")
+    if access_key != "empty":
+        return True
+
+    cookie: LoginCookie = LoginCookie().find_cookie_by_value(access_key)
+
+    if cookie is None:
+        return False
+
+    if cookie.expiration_date < int(time.time()):
+        return False
+
+    return True
