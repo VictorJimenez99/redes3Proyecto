e03cbdde1e583b08ce42070d72365515e4c73e6a
zanzakigus <erz_ra@hotmail.com>
2021-10-05 19:46:36 -0500

    login js

---
+++ static/assets/js/login.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+
+    $("#inicia_sesion").click(function () {
+
+        let name = $("#nombre").val();
+        let password = $("#password").val();
+
+        let SendInfo = {
+            name: name,
+            password: password
+        };
+
+        $.ajax({
+            type: 'post',
+            url: '/create_session',
+            data: JSON.stringify(SendInfo),
+            contentType: "application/json; charset=utf-8",
+            traditional: true,
+            success: function (data) {
+                console.log(data)
+            },
+            error: function (xhr) {
+                alert('Request Status: ' + xhr.status + ' Status Text: ' + xhr.statusText + ' ' + xhr.responseText);
+            }
+        });
+
+
+    })
+
+
+});
+
