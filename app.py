e487594047e67dcea619f8afb09d25c18d0af25a
VictorJimenez99 <jimenezvictor99@gmail.com>
2021-10-12 21:48:19 -0500

    Better formatting

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db.init_app(app)
 
 
+# -----------------------------INDEX----------------------------------------------
 @app.route('/')
 def index():
     if has_valid_session(request):
@@ -24,6 +25,7 @@ def index():
         return render_template("index.html")
 
 
+# -----------------------------LOGIN----------------------------------------------
 @app.route('/login')
 def login():
     if has_valid_session(request):
@@ -32,6 +34,21 @@ def login():
         return render_template("login.html")
 
 
+# -----------------------------LOGOUT--------------------------------------
+@app.route('/logout', methods=["POST"])
+def logout():
+    if request.method != 'POST':
+        return "not a post method", 400
+    if not has_valid_session(request):
+        return "Already logout", 208
+    response = make_response("")
+    value = request.cookies.get('access_key')
+    LoginCookie.logout_cookie(value)
+    response.delete_cookie('access_key')
+    return response, 200
+
+
+# -----------------------------CREATE_SESSION--------------------------------------
 @app.route('/create_session', methods=["POST"])
 def set_cookie():
     if request.method != 'POST':
@@ -45,7 +62,7 @@ def set_cookie():
     payload: dict = request.get_json(force=True)
     user_name = payload.get("name")
     password = payload.get("password")
-    user: SysUser = SysUser()
+    user: SysUser
     if SysUser.validate_credentials(user_name, password):
         user = SysUser.get_user_by_name(user_name)
     else:
@@ -62,24 +79,7 @@ def set_cookie():
     return response, 200
 
 
-@app.route('/check_cookie')
-def has_cookie():
-    value = request.cookies.get('login')
-    return f"login {value}"
-
-@app.route('/logout', methods=["POST"])
-def logout():
-    if request.method != 'POST':
-        return "not a post method", 400
-    if not has_valid_session(request):
-        return "Already logout", 208
-    response = make_response("")
-    value = request.cookies.get('access_key')
-    LoginCookie.logout_cookie(value)
-    response.delete_cookie('access_key')
-    return response, 200
-
-
+# ----------------------------ADD_SYS_USER----------------------------------------
 @app.route('/add_sys_user', methods=["POST"])
 def add_sys_user():
     if request.method != 'POST':
@@ -94,11 +94,44 @@ def add_sys_user():
     password = payload.get("new_password")
     if user_name is None or password is None:
         return "Unable to get params: Expected json with (new_name, new_password)", 406
+    possible_duplication = SysUser.get_user_by_name(user_name)
+    if possible_duplication:
+        return "Duplicated user; cannot add new user", 409
+
     SysUser.add_new_sys_user(user_name, password)
 
     response = make_response("")
     return response, 200
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ----------------------------------MAIN ---------------------------------------------
 if __name__ == '__main__':
     app.run(debug=True)
