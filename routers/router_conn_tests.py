c91aa4b5625b6e03ca44d581b603b95e3bcaa838
VictorJimenez99 <jimenezvictor99@gmail.com>
2021-10-24 11:56:57 -0500

    fixed test name

--- routers/router_conn_tests.py
+++ routers/router_conn_tests.py
@@ -5,12 +5,13 @@ from router_conn import RouterConnection
 
 class MyTestCase(unittest.TestCase):
     @staticmethod
-    def ping_pong():
+    def test_ping_pong():
         conn = RouterConnection("10.0.2.254", "root", "root")
         conn.start_transaction()
         conn.add_instructions_to_transaction(["show ip route"])
         value = conn.execute_transaction()
         print(value)
+        assert(1+1, 2)
 
 
 if __name__ == '__main__':
